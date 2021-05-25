da54b12659d40c3ccf27d55e0143e1b68f400d4f
Simon Saxtorph <simon@helloagent.dk>
2021-05-25 20:00:52 +0200

    fix: fixed addEnv on lambda function

---
+++ packages/aws-lambda/__tests__/lambda.unit.test.ts
@@ -0,0 +1,14 @@
+import { LambdaFunction, App, Stack } from "../../";
+
+describe("Lambda Unit Tests", () => {
+  test("When runtime is nodejs and there is no env added", () => {
+    const app = new App();
+    const stack = new Stack(app, "mystack");
+    const lambda = new LambdaFunction(stack, "mylambda", {
+      runtime: "nodejs14.x",
+      handler: "custom/handler",
+    });
+
+    expect(lambda.environment.AWS_NODEJS_CONNECTION_REUSE_ENABLED).toBe("1");
+  });
+});
