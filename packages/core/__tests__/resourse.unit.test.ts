1b2dc594ee55dfc9e87a9557e4c2ffcd9b5ce54d
Simon Saxtorph <simon@helloagent.dk>
2021-05-26 16:32:37 +0200

    feat: updated synthesize to omit private properties

---
+++ packages/core/__tests__/resourse.unit.test.ts
@@ -0,0 +1,22 @@
+import { BaseResource } from "../lib/resource";
+
+class TestResource extends BaseResource {
+  private _secret: string;
+  public name: string;
+
+  constructor() {
+    super(undefined as any, "");
+    this._secret = "This is a secret";
+    this.name = "Simon";
+  }
+  synth() {
+    return this.synthResource();
+  }
+}
+
+test("BaseResourse Synth", () => {
+  const resource = new TestResource();
+  console.log(resource.synth());
+  expect(resource.synth()._secret).toBe(undefined);
+  expect(resource.synth().name).toBe("Simon");
+});
