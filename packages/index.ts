29074b5a36078066f8b0933e11446ac7350a5dd8
Simon Saxtorph <simon@helloagent.dk>
2021-05-26 12:39:38 +0200

    chore: changed exports

--- packages/index.ts
+++ packages/index.ts
@@ -1,7 +1,8 @@
 export * from "./core";
-export * as sls_custom from "./sls-custom";
-export * as sls_plugin from "./sls-plugins";
-export * as sls_package from "./sls-package";
-export * as sls_provider from "./sls-provider";
+export * from "./sls-custom";
+export * from "./sls-plugins";
+export * from "./sls-package";
+export * from "./sls-provider";
+
 export * as aws_iam from "./aws-iam";
 export * as aws_lambda from "./aws-lambda";
